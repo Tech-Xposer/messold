2c63d40254acaa04f0aec5edf8f823522d4e3ecf
Ashutosh Sharma <asharma89505@gmail.com>
2024-06-16 12:46:00 +0530

    create shopify.service.js for Shopify API integration

---
+++ server/services/shopify.service.js
@@ -0,0 +1,28 @@
+import Shopify from "shopify-api-node";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { SHOPIFY_STORE_DOMAIN, SHOPIFY_API_KEY, SHOPIFY_ACCESS_TOKEN } =
+  process.env;
+
+if (!SHOPIFY_STORE_DOMAIN || !SHOPIFY_API_KEY || !SHOPIFY_ACCESS_TOKEN) {
+  throw new Error("Missing Shopify environment variables.");
+}
+
+const shopify = new Shopify({
+  shopName: SHOPIFY_STORE_DOMAIN,
+  accessToken: SHOPIFY_ACCESS_TOKEN,
+});
+if (process.env.ENV === "dev") {
+  async function testShopifyConnection() {
+    try {
+      const shop = await shopify.shop.get();
+      console.log(`Connected to Shopify store: ${shop.name}`);
+    } catch (error) {
+      console.error("Error connecting to Shopify:", error.message);
+    }
+  }
+  testShopifyConnection();
+}
+
+export default shopify;
