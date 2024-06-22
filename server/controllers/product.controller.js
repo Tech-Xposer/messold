94f424fa3e921fa5463fb2c268e85fc38a4f7605
Ashutosh Sharma <asharma89505@gmail.com>
2024-06-22 23:37:47 +0530

    add pagination, add-to-cart feature

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,56 +1,43 @@
 import ApiResponse from "../handlers/response.handler.js";
 import shopify from "../services/shopify.service.js";
-
+import ApiError from "../handlers/error.handler.js";
+import User from "../models/user.model.js";
+import Cart from "../models/cart.model.js";
 export const getProducts = async (req, res) => {
   try {
-    // Extract and validate the limit and page parameters from the query
     let { limit, page } = req.query;
     limit = parseInt(limit, 10);
     page = parseInt(page, 10);
-
-    // Default to 10 if limit is not provided or invalid
     if (isNaN(limit) || limit <= 0) {
       limit = 10;
     }
-
-    // Default to 1 if page is not provided or invalid
     if (isNaN(page) || page <= 0) {
       page = 1;
     }
 
-    console.log(`Fetching products with limit: ${limit} and page: ${page}`);
-
-    // Initialize the parameters for fetching products
     let params = { limit };
-
-    // Loop through pages to get the correct set of products
+    console.log(params);
     for (let i = 1; i < page; i++) {
       const products = await shopify.product.list(params);
       params = products.nextPageParameters;
 
-      // If there are no more pages, break early
       if (!params) {
-        return ApiResponse.notFound(res, "No more products found");
+        throw new ApiError(404, "No more products found");
       }
     }
 
-    // Fetch the products for the requested page
     const products = await shopify.product.list(params);
 
-    // Log the number of products fetched
-    console.log(`Number of products fetched: ${products.length}`);
-
-    // Check if products are found and respond accordingly
     if (products && products.length > 0) {
+      const totalProducts = await shopify.product.count();
       ApiResponse.success(res, 200, "Products found successfully", {
-        count: products.length,
+        count: totalProducts,
         products,
       });
     } else {
-      ApiResponse.notFound(res, "No products found");
+      throw new ApiError(404, "No products found");
     }
   } catch (error) {
-    // Log the error and send error response
     console.error("Error fetching products:", error);
     ApiResponse.error(res, error.message, error.statusCode || 500);
   }
@@ -60,20 +47,73 @@ export const getProductById = async (req, res) => {
   try {
     const { id } = req.params;
     const product = await shopify.product.get(id);
-    if (product) {
-      return ApiResponse.success(
-        res,
-        200,
-        "Product found successfully",
-        product,
-      );
+    if (!product) {
+      throw new ApiError(404, "No more products found");
     }
-    return ApiResponse.notFound(res, "Product not found");
+    return ApiResponse.success(res, 200, "Product found successfully", product);
   } catch (err) {
     return ApiResponse.error(
       res,
       err.message || "Internal Server Error",
       err.statusCode || 500,
     );
+  }
+};
+
+export const addToCart = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = req.user.id;
+    const { quantity } = req.body;
+
+    if (!id) {
+      throw new ApiError(400, "Product ID is required!");
+    }
+
+    const product = await shopify.product.get(id);
+    if (!product) {
+      throw new ApiError(404, "Product not found!");
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new ApiError(404, "User not found!");
+    }
+
+    let cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+      cart = new Cart({
+        user: userId,
+        products: [
+          {
+            id: product.id,
+            quantity: quantity || 1,
+          },
+        ],
+      });
+    } else {
+      console.log("users cart found", cart);
+      const productIndex = cart.products.findIndex((p) => p.id === id);
+      console.log(productIndex);
+      if (productIndex > -1) {
+        cart.products[productIndex].quantity += quantity || 1;
+      } else {
+        cart.products.push({
+          id: product.id,
+          quantity: quantity || 1,
+        });
+      }
+    }
+
+    await cart.save();
+
+    return ApiResponse.success(
+      res,
+      200,
+      "Product added to cart successfully",
+      cart,
+    );
+  } catch (error) {
+    return ApiResponse.error(res, error.message, error.statusCode || 500);
   }
 };
